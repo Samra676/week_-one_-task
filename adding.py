e03051b6a381dddf485de3c799e865a64d6b22d1
Samra naz <naazsamra01@gmail.com>
2025-06-26 23:40:16 +0500

    Add files via upload

---
+++ adding.py
@@ -0,0 +1,4 @@
+x=int(input("please enter first num:"))
+y=int(input("plz enter the second num:"))
+sum_numbers=x+y
+print("the sum of",  x, "and",y,"is:", sum_numbers)
---
+++ myself.py
@@ -0,0 +1,10 @@
+name ="my name is samra naz "
+age="my age is 23"
+hobby="my hobby is exploring some new things"
+#why we want learn python 
+learnpython ="python  is used in many area data science ML web development and more   east to learn  python skills increases job opportunities"
+print(name)
+print(age)
+print(hobby)
+print(learnpython)
+
